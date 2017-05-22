bef7e86c08092dec66a684d150554d995e8703ad
Nick Meyer <meyersoftware@gmail.com>
2017-05-22 14:11:51 -0400

    woohoo

--- PojoRestfulService/Controllers/ValuesController.cs
+++ PojoRestfulService/Controllers/ValuesController.cs
@@ -53,9 +53,53 @@ namespace PojoRestfulService.Controllers
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public double[] Get(int year, double team1, double team2)
         {
-            return "value";
+            string sFileContents = "";
+
+            var url = "http://pwp.apphb.com//mm2017Matchups.csv";
+            //var url = "http://localhost:50209/mm2017Matchups.csv";
+
+            WebClient wc = new WebClient();
+
+            using (StreamReader oStreamReader = new StreamReader(wc.OpenRead(url)))
+            {
+                sFileContents = oStreamReader.ReadToEnd();
+            }
+
+            List<string[]> oCsvList = new List<string[]>();
+
+            string[] sFileLines = sFileContents.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string sFileLine in sFileLines)
+            {
+                oCsvList.Add(sFileLine.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            int iColumnNumber = 0;
+            int iRowNumber = 0;
+
+            for (iColumnNumber=0; iColumnNumber <= oCsvList.Count; iColumnNumber++)
+            {
+                if (oCsvList[iColumnNumber][iRowNumber].ToString() == year.ToString() + "_" + team1.ToString() + "_" + team2.ToString())
+                {
+                    break;
+                }
+            }
+
+            double[] values = new double[14];
+
+            for (iRowNumber = 2; iRowNumber < 15; iRowNumber++)
+            {
+                values[iRowNumber - 2] = double.Parse(oCsvList[iColumnNumber][iRowNumber]);
+            }
+
+            grid_f6497669_4fd1_4bd0_b8b3_9ad79c496aa2_model_1 test = new grid_f6497669_4fd1_4bd0_b8b3_9ad79c496aa2_model_1();
+
+            double[] preds = new double[14];
+
+            test.score0(values, preds);
+
+            return preds;
         }
 
         // POST api/values
